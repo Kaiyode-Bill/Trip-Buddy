a973f22333d0fbde5e15c9c265b012e82ac5ab61
Kaiyode-Chris <chris@kaiyode.com>
2016-04-18 23:15:40 -0700

    Changed ViewController to MoneyViewController

---
+++ Trip Buddy/MoneyViewController.swift
@@ -0,0 +1,11 @@
+//Trip Buddy
+//MoneyViewController.swift
+//© 2016 Kaiyode Software
+
+import UIKit
+
+class MoneyViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
