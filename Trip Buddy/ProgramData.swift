220ca343ffbcb4364903810dea23a205c8401922
Kaiyode-Chris <chris@kaiyode.com>
2017-12-19 01:24:20 -0700

    Old: Updated the data types for the Program Data attributes for both the model & code.
    Note: Make sure to update ProgramData code usage to reflect this!

--- Trip Buddy/ProgramData.swift
+++ Trip Buddy/ProgramData.swift
@@ -1,30 +1,30 @@
 //Trip Buddy
 //ProgramData.swift
-//© 2016 Kaiyode Software
+//(c) 2018 Kaiyode Software
 
 import Foundation
 import CoreData
 
 class ProgramData: NSManagedObject {
-	@NSManaged var originCountry: NSNumber
-	@NSManaged var travelCountry: NSNumber
-	@NSManaged var countryExchangeRate: NSNumber
+	@NSManaged var originCountry: Int
+	@NSManaged var travelCountry: Int
+	@NSManaged var countryExchangeRate: Double
 	@NSManaged var countryExchangeDate: String
-	@NSManaged var showHelpAtStartup: NSNumber
-	@NSManaged var exchangeAmount: NSNumber
-	@NSManaged var exchangePercentage: NSNumber
-	@NSManaged var exchangeFee: NSNumber
-	@NSManaged var exchangeOutcome: NSNumber
-	@NSManaged var gasUnit: NSNumber
-	@NSManaged var gasEquivalentUnit: NSNumber
-	@NSManaged var gasRate: NSNumber
-	@NSManaged var gasAmount: NSNumber
-	@NSManaged var gasOutcome: NSNumber
-	@NSManaged var mealAmount: NSNumber
-	@NSManaged var mealPercentage: NSNumber
-	@NSManaged var mealPeople: NSNumber
-	@NSManaged var equalDistanceAmount: NSNumber
-	@NSManaged var equalDistanceUnit: NSNumber
-	@NSManaged var equalTemperatureAmount: NSNumber
-	@NSManaged var equalTemperatureUnit: NSNumber
+	@NSManaged var showHelpAtStartup: Bool
+	@NSManaged var exchangeAmount: Double
+	@NSManaged var exchangePercentage: Int
+	@NSManaged var exchangeFee: Double
+	@NSManaged var exchangeOutcome: Double
+	@NSManaged var gasUnit: Int
+	@NSManaged var gasEquivalentUnit: Int
+	@NSManaged var gasRate: Double
+	@NSManaged var gasAmount: Double
+	@NSManaged var gasOutcome: Double
+	@NSManaged var mealAmount: Double
+	@NSManaged var mealPercentage: Int
+	@NSManaged var mealPeople: Int
+	@NSManaged var equalDistanceAmount: Double
+	@NSManaged var equalDistanceUnit: Int
+	@NSManaged var equalTemperatureAmount: Double
+	@NSManaged var equalTemperatureUnit: Int
 }
